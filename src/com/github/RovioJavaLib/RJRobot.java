270eb58207399c39af0368386e21d7c0f9090918
Matthew Barulic <matthew.barulic@gmail.com>
2012-06-28 13:39:35 -0400

    Project Setup

---
+++ src/com/github/RovioJavaLib/RJRobot.java
@@ -0,0 +1,21 @@
+/**
+ * @author Brooke Bosley
+ * 
+ * 
+ * 
+ */
+package com.github.RovioJavaLib;
+
+/**
+ *
+ */
+public class RJRobot {
+
+	/**
+	 * 
+	 */
+	public RJRobot(String IP) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
